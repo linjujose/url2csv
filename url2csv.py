d74444f49658ce648ca8491aeb6dd00cea1ea94e
linjujose <linjujose@gmail.com>
2015-04-21 15:38:03 -0700

    Create url2csv.py
    # If the API returns JSON data json.loads() can be used.
    # Here I am using some custom string manipulation using string.split("pattern")

---
+++ url2csv.py
@@ -0,0 +1,44 @@
+import pycurl
+from cStringIO import StringIO
+import csv
+
+try:
+    buffer = StringIO()
+    d = pycurl.Curl()
+    d.setopt(d.URL, 'http://host:port/getResultsForYourData?parameters=your%20values')
+    d.setopt(d.WRITEDATA, buffer)
+    d.perform()
+    if(buff.getvalue() == ""): # if no response, retry 3 times
+        for x in range(0, 3):
+            d.perform()
+    d.close()
+except:
+    print "Error getting data from the API"
+
+data = buffer.getvalue()
+
+
+ids = []
+dates = []
+id1 = data.split("u'access_key': ")
+date1 = data.split("u'date_created': ")
+
+for i in range(len(id1) -1): #split creates an extra string at the begining
+    id2 = id1[i+1].split(", 'next/following_pattern':")
+    ids.append(id2[0])
+    date2 = date1[i+1].split("}")
+    dates.append(date2[0])
+
+# Write data to CSV file.
+try:
+    writeFile = csv.writer(open('data.csv', 'wb', buffering=0))
+    writeFile.writerow([('id'), ('date_created'), ('state')])
+except:
+    print "Error creating csv file for writing"
+    raise
+
+for index in range(len(id1) -1):
+    writeFile.writerow([(ids[index]),(dates[index]),('CA')])
+
+
+print 'The CSV file is saved into the local directory as {}'.format('data.csv')
